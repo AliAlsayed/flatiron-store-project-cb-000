bf1d8e8f2e438a58a1d6369650d957824982bb20
Ali Alsayed <a_alsayed87@hotmail.com>
2019-08-18 10:58:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def redirecrt_to_store
-    redirecrt_to store_path
+    redirect_to store_path
   end
 end
