e24f6b2fc649bfd05194e76512445a858108a0cf
Ali Alsayed <a_alsayed87@hotmail.com>
2019-08-18 11:04:14 +0000

    Automatically backed up by Learn

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,12 @@ class Item < ActiveRecord::Base
   has_many :line_items
   has_many :carts, through: :line_items
   belongs_to :category
+
+  def self.available_items
+    Item.all.map do |item|
+      if item.inventory > 0
+        item
+      end
+    end
+  end
 end
